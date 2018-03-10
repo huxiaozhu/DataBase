4d8cb6d6f19c0fd7ac1380d1c2193d8ffe3fd43c
liusmallpig <chenhuixueba@163.com>
2018-03-10 16:04:20 +0800

    Initial commit

---
+++ app/src/main/java/com/liuxiaozhu/database/MainActivity.java
@@ -0,0 +1,17 @@
+package com.liuxiaozhu.database;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 数据库架构设计
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
